28e596a005bef867d4c3ac49fea2ffeec75fe507
Nick Iliev <nikolay.iliev@telerik.com>
2018-01-03 09:43:31 +0200

    refactor: tslint'

--- NativeScript/issue_5226/app/main-page.ts
+++ NativeScript/issue_5226/app/main-page.ts
@@ -6,11 +6,10 @@ export function navigatingTo(args: EventData) {
     let appContext = <android.content.Context>application.android.context;
     let windowManager = appContext.getSystemService(android.content.Context.WINDOW_SERVICE);
     let rotation = windowManager.getDefaultDisplay().getRotation();
-
     console.log(rotation);
 
     application.on(application.orientationChangedEvent, (args) => {
         console.log("orientationChangedEvent");
-        console.log(args.newValue);
+        console.log(args.newValue); // landscape or prtrait
     });
 }
