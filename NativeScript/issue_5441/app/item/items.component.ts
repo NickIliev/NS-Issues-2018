6ac25f9ab41303f7056a82705b8984d8fbc9118a
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-02-20 12:59:15 +0200

    add]

--- NativeScript/issue_5441/app/item/items.component.ts
+++ NativeScript/issue_5441/app/item/items.component.ts
@@ -3,9 +3,9 @@ import { Component } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ListView [items]="[1, 2, 3, 4, 5, 6, 7, 8, 9]" rowHeight="40">
+    <ListView [items]="[1, 2, 3, 4, 5, 6, 7, 8]" rowHeight="40">
         <ng-template>
-            <Label text="Label" style.background-color="#bbbbbb"></Label>
+            <Label height="40" text="Label" style.background-color="#bbbbbb"></Label>
         </ng-template>
     </ListView>
   `
