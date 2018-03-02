aa358aa01dc6b4b699e2f24a446961522a10b925
NickIliev <nikolay.iliev@telerik.com>
2018-03-02 11:32:13 +0200

    add

--- nativescript-camera/issue_88/app/main-view-model.ts
+++ nativescript-camera/issue_88/app/main-view-model.ts
@@ -17,4 +17,9 @@ export class HelloWorldModel extends Observable {
         });
     }
 
+    // picture_path1 without savetToGallery (default value false)
+    // picturePath_1: /storage/emulated/0/Android/data/org.nativescript.issue88/files/NSIMG_20180302_112625.jpg
+
+    // picture_path1 with savetToGallery (default value false)
+    // picturePath_1: /storage/emulated/0/Android/data/org.nativescript.issue88/files/NSIMG_20180302_112753.jpg
 }
