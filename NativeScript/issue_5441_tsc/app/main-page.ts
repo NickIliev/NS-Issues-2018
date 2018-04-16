c78df7c50cc1d4f75345181a50f7156cae085d92
Nikolay Iliev <Nikolay.Iliev@telerik.com>
2018-04-16 14:10:14 +0300

    add

---
+++ NativeScript/issue_5441_tsc/app/main-page.ts
@@ -0,0 +1,11 @@
+
+
+import { EventData } from 'data/observable';
+import { Page } from 'ui/page';
+import { StackLayout } from "ui/layouts/stack-layout";
+import { ListView, ItemEventData } from "ui/list-view";
+
+export function navigatingTo(args: EventData) {
+
+    let page = <Page>args.object;
+}
