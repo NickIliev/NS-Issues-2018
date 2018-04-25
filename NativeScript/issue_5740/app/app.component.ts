165ad05587d3e91bb0f47b195a8ee498781123e0
NickIliev <nikolay.iliev@telerik.com>
2018-04-25 09:49:48 +0300

    add

---
+++ NativeScript/issue_5740/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <!-- Your UI components go here -->
+    <Label text="some text"></Label>
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
